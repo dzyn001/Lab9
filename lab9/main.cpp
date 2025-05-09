13face92d5402d6c469f59d5a43fbc3e3952de33
dzyn001 <oksanamikhalyncuk@email.com>
2025-05-09 09:35:46 +0300

    completed task2

--- lab9/main.cpp
+++ lab9/main.cpp
@@ -2,13 +2,15 @@
 #include <thread>
 #include <vector>
 
+int counter = 0;
+
 int main() {
     std::vector<std::thread> threads;
 
     for (int i = 1; i <= 3; ++i) {
         threads.emplace_back([i]() {
-            int result = i * i;
-            std::cout << "Thread " << i << ": " << i << "^2 = " << result << std::endl;
+            counter += i;
+            std::cout << "Thread " << i << ": counter = " << counter << std::endl;
         });
     }
 
@@ -18,3 +20,4 @@ int main() {
 
     return 0;
 }
+
